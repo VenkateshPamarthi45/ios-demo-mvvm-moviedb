2235e8e7b87871e8701d716746169e6978b22a97
venkateshpamarthi <Venkatesh.pamarthi@erosnow.com>
2019-06-25 20:06:31 +0530

    Used rx swift interaction between view model, view and repository

--- MovieDBTests/MovieListingTests/PresenterTests/MovieListingPresenterImplTest.swift
+++ MovieDBTests/MovieListingTests/PresenterTests/MovieListingPresenterImplTest.swift
@@ -8,30 +8,40 @@
 
 import XCTest
 @testable import MovieDB
+import RxSwift
 
 class MovieListingPresenterImplTest: XCTestCase {
     
     var mockClass : MockMovieListingInteractorImpl!
-    var sut : MovieListingPresenterImpl!
+    var sut : MovieListingViewModelImpl!
+    private var disposableBag = DisposeBag()
+    
     
     override func setUp() {
         let movieListingService = MockMovieListingServiceImpl()
         mockClass = MockMovieListingInteractorImpl(movieListingService: movieListingService)
-        sut = MovieListingPresenterImpl(movieInteractor: mockClass)
+        sut = MovieListingViewModelImpl(movieRepository: mockClass)
     }
 
 
     func test_ResponseIsNotNil() {
-        sut?.fetchMovies(pageId: "1", closure: { (response) in
-                XCTAssertNotNil(response)
-        })
+        sut?.fetchMovies(pageId: "1").subscribe(onSuccess: { (response) in
+            XCTAssertNotNil(response)
+        }, onError: { (err) in
+            print(err)
+            XCTAssertNil(err)
+        }).disposed(by: disposableBag)
+        
     }
     
     func test_ifMovieListingInteractorMethodIsCalled() {
-        sut?.fetchMovies(pageId: "1", closure: { (response) in
-            
-        })
-        XCTAssertTrue(mockClass.didFetchMoviesApiCalled)
+        sut?.fetchMovies(pageId: "1").subscribe(onSuccess: { (response) in
+            print(response)
+            XCTAssertTrue(self.mockClass.didFetchMoviesApiCalled)
+        }, onError: { (err) in
+            XCTAssertNil(err)
+        }).disposed(by: disposableBag)
+        
     }
 
 }
